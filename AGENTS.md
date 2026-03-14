f09ee4524158da6ae0c8443846c234289618aecb
cesarszv <zarventt@icloud.com>
2026-03-13 23:00:35 -0400

    feat: implement C++ program to generate a Fibonacci-like numeric series based on user input.

---
+++ AGENTS.md
@@ -0,0 +1,16 @@
+# Series Numerica
+
+este es un practico para la asignatura `Introduction to Programming` (SIS-111) en la "Universidad Católica Boliviana San Pablo" (UCB) en el semestre 1-2026
+
+- [ ] identificar la fórmula o patrón de cada serie
+- [ ] resolver cada serie con `ciclo for` en `C++`
+
+# Metodologia Usada
+
+ejercicio.cpp{
+aqui se encontrara el codigo del ejercicio resuelto, debe estar pulido, con documentacion usando el estandar
+}
+
+ejercicio.md{
+aqui se encontrara el "plan" para resolver el ejercicio, el razonamiento y un diagrama de flujo en mermaid.js
+}
---
+++ exercises/1.cpp
@@ -0,0 +1,46 @@
+/*
+SERIE NUMERICA = 4, 5, 9, 14, 23, 37, ...
+
+Fibonacci
+*/
+
+#include <iostream>
+
+int main() {
+    // definir la longitud de la serie
+    int longitud_serie;
+    std::cout << "introduce la longitud de tu serie: ";
+    std::cin >> longitud_serie;
+
+    if(longitud_serie <= 2) {
+        std::cout << "la longitud debe ser mas de 2 digitos";
+        return 1;
+    }
+
+    // definir variables con las que trabajaremos
+    int primer_termino;
+    std::cout << "introduce tu primer termino (el ejercicio usa el 4): ";
+    std::cin >> primer_termino;
+
+
+    int segundo_termino;
+    std::cout << "introduce tu primer termino (el ejercicio usa el 5): ";
+    std::cin >> segundo_termino;
+
+    int siguiente;
+
+    // primeros terminos
+    std::cout << primer_termino << ", ";
+    std::cout << segundo_termino;
+
+    //
+    for(int i = 2; i < longitud_serie; i++){
+        siguiente = primer_termino + segundo_termino;
+        std::cout << "," << siguiente;
+
+        primer_termino = segundo_termino;
+        segundo_termino = siguiente;
+    }
+
+    return 0;
+}
